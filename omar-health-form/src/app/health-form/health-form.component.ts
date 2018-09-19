f848f340a9970064434c79e56f886e42bc2ff63b
mcampaz3 <mcampaz@gatech.edu>
2018-09-19 11:55:29 -0700

    added swith to health form

--- omar-health-form/src/app/health-form/health-form.component.ts
+++ omar-health-form/src/app/health-form/health-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router'
 import { Form } from '@angular/forms'
+import {forEach} from "@angular/router/src/utils/collection";
 
 @Component({
   selector: 'app-health-form',
@@ -10,11 +11,23 @@ import { Form } from '@angular/forms'
 export class HealthFormComponent implements OnInit {
 
   constructor(private router: Router) {}
-  createHealthSurvey( ){
-    this.router.navigate(['health-program1']);
+  createHealthSurvey(healthForm){
+    debugger;
+    console.log(healthForm);
+    let myNumber = 3;
+    switch (myNumber) {
+      case 1:
+        this.router.navigate(['health-program1']);
+      case 2:
+        this.router.navigate(['health-program2']);
+      case 3:
+        this.router.navigate(['health-program3']);
+      default:
+        this.router.navigate(['health-program4']);
+    }
   }
   createAthlete(){
-    this.router.navigate(['health-program1']);
+
   }
 
   ngOnInit() {
